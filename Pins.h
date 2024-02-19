060ad8cd88da7e2f7b6926dcfc0014c248a2f9ae
Nicholas David <nicholas.carreno@cumplimos.co>
2024-02-19 11:02:40 -0500

    v0.1

---
+++ Pins.h
@@ -0,0 +1,32 @@
+// ? *************** MOTORS ***************
+
+// ? ************ MOTORS Y ************
+// ? *********** MOTOR Y1 ***********
+// ? ******* PINS
+#define MOTOR_Y1_STEP PB_0
+#define MOTOR_Y1_DIR PB_1
+#define MOTOR_Y1_ENABLE PB_2
+// ? *********** MOTOR Y2 ***********
+// ? ******* PINS
+#define MOTOR_Y2_STEP PA_0
+#define MOTOR_Y2_DIR PA_1
+#define MOTOR_Y2_ENABLE PA_4
+
+// ? ************ MOTORS Z ************
+// ? *********** MOTOR Z1 ***********
+// ? ******* PINS
+#define MOTOR_Z1_STEP PB_3
+#define MOTOR_Z1_DIR PB_4
+#define MOTOR_Z1_ENABLE PB_5
+// ? *********** MOTOR Z2 ***********
+// ? ******* PINS
+#define MOTOR_Z2_STEP PA_5
+#define MOTOR_Z2_DIR PA_6
+#define MOTOR_Z2_ENABLE PA_7
+
+// ? ************ MOTORS X ************
+// ? *********** MOTOR X1 ***********
+// ? ******* PINS
+#define MOTOR_X1_STEP PA_8
+#define MOTOR_X1_DIR PA_13
+#define MOTOR_X1_ENABLE PA_14
